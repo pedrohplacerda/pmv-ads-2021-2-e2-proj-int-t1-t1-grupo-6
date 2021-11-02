3cd1d87d1315c289d97eddfe4b87cd9df2d14744
pedrohplacerda <79854133+pedrohplacerda@users.noreply.github.com>
2021-11-01 22:21:50 -0300

    Add files via upload

---
+++ dump-puc_ads_gestor-202110270806.sql
@@ -0,0 +1,182 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: puc_ads_gestor
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tb_empresa`
+--
+
+DROP TABLE IF EXISTS `tb_empresa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_empresa` (
+  `nome` char(20) DEFAULT NULL,
+  `cnpj` decimal(14,0) DEFAULT NULL,
+  `id_empresa` char(8) NOT NULL,
+  PRIMARY KEY (`id_empresa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_empresa`
+--
+
+LOCK TABLES `tb_empresa` WRITE;
+/*!40000 ALTER TABLE `tb_empresa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_empresa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_endereco`
+--
+
+DROP TABLE IF EXISTS `tb_endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_endereco` (
+  `rua` char(15) DEFAULT NULL,
+  `cidade` char(15) DEFAULT NULL,
+  `estado` char(15) DEFAULT NULL,
+  `cep` char(15) NOT NULL,
+  `id_endereco` char(8) DEFAULT NULL,
+  PRIMARY KEY (`cep`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_endereco`
+--
+
+LOCK TABLES `tb_endereco` WRITE;
+/*!40000 ALTER TABLE `tb_endereco` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_endereco` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_estoque`
+--
+
+DROP TABLE IF EXISTS `tb_estoque`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_estoque` (
+  `id_produto` char(8) DEFAULT NULL,
+  `qntd` decimal(10,0) DEFAULT NULL,
+  `em_estoque` tinyint(1) DEFAULT NULL,
+  KEY `FK_tb_Produto_tb_Estoque` (`id_produto`),
+  CONSTRAINT `FK_tb_Produto_tb_Estoque` FOREIGN KEY (`id_produto`) REFERENCES `tb_produto` (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_estoque`
+--
+
+LOCK TABLES `tb_estoque` WRITE;
+/*!40000 ALTER TABLE `tb_estoque` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_estoque` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_financeiro`
+--
+
+DROP TABLE IF EXISTS `tb_financeiro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_financeiro` (
+  `entrada` decimal(20,0) DEFAULT NULL,
+  `saida` decimal(20,0) DEFAULT NULL,
+  `mesReferente` date DEFAULT NULL,
+  `resultado` decimal(20,0) DEFAULT NULL,
+  `id_operacao` char(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_financeiro`
+--
+
+LOCK TABLES `tb_financeiro` WRITE;
+/*!40000 ALTER TABLE `tb_financeiro` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_financeiro` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_produto`
+--
+
+DROP TABLE IF EXISTS `tb_produto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_produto` (
+  `nome_produto` char(25) DEFAULT NULL,
+  `preco_produto` decimal(8,0) DEFAULT NULL,
+  `categoria` char(8) DEFAULT NULL,
+  `id_produto` char(8) NOT NULL,
+  PRIMARY KEY (`id_produto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_produto`
+--
+
+LOCK TABLES `tb_produto` WRITE;
+/*!40000 ALTER TABLE `tb_produto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_produto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tb_usuario` (
+  `usuario` char(20) DEFAULT NULL,
+  `senha` char(8) DEFAULT NULL,
+  `permissao` char(7) DEFAULT NULL,
+  `id_usuario` char(8) NOT NULL,
+  `nome_empregado` char(20) DEFAULT NULL,
+  PRIMARY KEY (`id_usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_usuario`
+--
+
+LOCK TABLES `tb_usuario` WRITE;
+/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'puc_ads_gestor'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-10-27  8:07:00
