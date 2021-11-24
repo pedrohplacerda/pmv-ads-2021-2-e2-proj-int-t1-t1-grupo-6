c2e710412a9a8a3bb36568910aae42a4899f6f77
pedrohplacerda <pedrohenrique73490@gmail.com>
2021-11-23 22:00:29 -0300

    Refatorando e criando a classe StatusController

--- src/main/java/puc/minas/demo/App.java
+++ src/main/java/puc/minas/demo/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoGestorApplication {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoGestorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 }
