5ecda94957e29c90733da9e93fffc6ff58accfbe
pedrohplacerda <pedrohenrique73490@gmail.com>
2021-11-24 20:45:26 -0300

    Melhorando o método no controller da estoque.

--- src/main/java/puc/minas/demo/controller/EstoqueController.java
+++ src/main/java/puc/minas/demo/controller/EstoqueController.java
@@ -2,6 +2,7 @@ package puc.minas.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import puc.minas.demo.model.EstoqueModel;
 import puc.minas.demo.repository.EstoqueRepository;
@@ -15,12 +16,15 @@ public class EstoqueController {
     @Autowired
     private EstoqueRepository repository;
 
-//    @PatchMapping(path = "/atualizar")
-//    public com.sample.EstoqueModel atualizarEstoque(@RequestHeader Double idProduto, @RequestHeader Double qntd) {
-//        if(repository.existsById(idProduto)) {
-//            return repository.;
-//        }
-//    }
+    @PatchMapping(path = "/atualizar")
+    public ResponseEntity atualizarEstoque(@RequestHeader Double idProduto, @RequestBody EstoqueModel estoqueModel) {
+        if(repository.existsById(idProduto)) {
+            String msg = "Produto atualizado com sucesso no estoque.";
+            repository.save(estoqueModel);
+            return ResponseEntity.ok().body(msg);
+        }
+        return null;
+    }
 
     @PostMapping(path = "/inserir")
     public EstoqueModel inserirProdutoNoEstoque(@RequestBody EstoqueModel estoque) {
@@ -28,7 +32,7 @@ public class EstoqueController {
     }
 
     @DeleteMapping(path = "/deletar")
-    public void deletarDoEstoque(@RequestHeader Double idProduto) {
+    public void deletarProdutoNoEstoque(@RequestHeader Double idProduto) {
         if(repository.existsById(idProduto)) {
             repository.deleteById(idProduto);
         }
